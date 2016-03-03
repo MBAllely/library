358eb4e9de21ac125bdfa124b57653d26d2a89fe
Marika Allely and Mollie Bootsma <mollie.bootsma@gmail.com>
2016-03-02 17:08:38 -0800

    add save and getAll functions to Patron.php

---
+++ src/Patron.php
@@ -0,0 +1,101 @@
+<?php
+    class Patron
+    {
+        private $patron_name;
+        private $id;
+
+        function __construct($patron_name, $id = null)
+        {
+            $this->patron_name = $patron_name;
+            $this->id = $id;
+        }
+
+        function setPatronName($new_patron_name)
+        {
+            $this->patron_name = $new_patron_name;
+        }
+
+        function getPatronName()
+        {
+            return $this->patron_name;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+
+        function save()
+        {
+            $GLOBALS['DB']->exec("INSERT INTO patrons (patron_name) VALUES ('{$this->getPatronName()}');");
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+
+        static function getAll()
+        {
+            $returned_patrons = $GLOBALS['DB']->query("SELECT * FROM patrons;");
+            $patrons = [];
+
+            foreach($returned_patrons as $patron) {
+                $patron_name = $patron['patron_name'];
+                $id = $patron['id'];
+                $new_patron = new Patron($patron_name, $id);
+                array_push($patrons, $new_patron);
+            }
+            return $patrons;
+        }
+
+        function update($new_patron_name)
+        {
+            $GLOBALS['DB']->exec("INSERT INTO patrons SET patron_name = '{$new_patron_name}';");
+            $this->setPatronName($new_patron_name);
+        }
+
+        static function find($search_id)
+        {
+            $found_patron = null;
+            $patrons = Patron::getAll();
+            foreach($patrons as $patron) {
+                if ($patron->getId() == $search_id) {
+                    $found_patron = $patron;
+                }
+            }
+            return $found_patron;
+        }
+
+//Move this to Checkout.php
+        function addPatron($patron)
+        {
+            $GLOBALS['DB']->exec("INSERT INTO checkout (patron_id) VALUES ({$patron->getId()}, {$this->getId()});");
+        }
+//
+        // function getAuthors()
+        // {
+        //     $found_authors = $GLOBALS['DB']->query("SELECT authors.* FROM books
+        //         JOIN author_book ON (books.id = author_book.book_id)
+        //         JOIN authors ON (author_book.author_id = authors.id)
+        //         WHERE books.id = {$this->getId()};");
+        //
+        //     $authors = [];
+        //     foreach($found_authors as $author) {
+        //
+        //         $author_name = $author['author_name'];
+        //         $id = $author['id'];
+        //         $new_author = new Author($author_name, $id);
+        //         array_push($authors, $new_author);
+        //     }
+        //     return $authors;
+        // }
+        //
+        static function deleteAll()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM patrons;");
+        }
+
+        // function deleteOneBook()
+        // {
+        //     $GLOBALS['DB']->exec("DELETE FROM book WHERE id = {$this->getId()};");
+        //     $GLOBALS['DB']->exec("DELETE FROM author_book WHERE book_id = {$this->getId()};");
+        // }
+
+    }
---
+++ tests/PatronTest.php
@@ -0,0 +1,127 @@
+<?php
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    $server = 'mysql:host=localhost;dbname=library_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    require_once __DIR__ . '/../src/Patron.php';
+
+    class PatronTest extends PHPUnit_Framework_TestCase
+    {
+        protected function tearDown()
+        {
+            Patron::deleteAll();
+        }
+
+        function test_getInfo()
+        {
+            //Arrange
+            $patron_name = "Winston Rowntree";
+            $id = 1;
+            $test_patron = new Patron($patron_name, $id);
+
+            //Act
+            $result1 = $test_patron->getPatronName();
+            $result2 = $test_patron->getId();
+
+            //Assert
+            $this->assertEquals($patron_name, $result1);
+            $this->assertEquals($id, $result2);
+        }
+
+        function test_save()
+        {
+            //Arrange
+            $patron_name = "Maragaret Atwood";
+            $test_patron = new Patron($patron_name);
+            $test_patron->save();
+
+            //Act
+            $result = Patron::getAll();
+
+            //Assert
+            $this->assertEquals([$test_patron], $result);
+        }
+
+        function test_getAll()
+        {
+            //Arrange
+            $patron_name = "Maragaret Atwood";
+            $test_patron = new Patron($patron_name);
+            $test_patron->save();
+
+            $patron_name2 = "Philip Pullman";
+            $test_patron2 = new Patron($patron_name2);
+            $test_patron2->save();
+
+            //Act
+            $result = Patron::getAll();
+
+            //Assert
+            $this->assertEquals([$test_patron, $test_patron2], $result);
+        }
+
+        // function test_addBook()
+        // {
+        //     //Arrange
+        //     $author_name = "Philip Pullman";
+        //     $test_author = new Author($author_name);
+        //     $test_author->save();
+        //
+        //     $title = "Golden Compass";
+        //     $copies = 3;
+        //     $test_book = new Book($title, $copies);
+        //     $test_book->save();
+        //
+        //     //Act
+        //     $test_author->addBook($test_book);
+        //
+        //
+        //     //Assert
+        //     $this->assertEquals($test_author->getBooks(), [$test_book]);
+        //
+        // }
+        //
+        //
+        // function test_getBooks()
+        // {
+        //     //Arrange
+        //     $author_name = "Philip Pullman";
+        //     $test_author = new Author($author_name);
+        //     $test_author->save();
+        //
+        //     $title = "Golden Compass";
+        //     $copies = 3;
+        //     $id = 1;
+        //     $test_book = new Book($title, $copies, $id);
+        //     $test_book->save();
+        //
+        //     $title2 = "The Subtle Knife";
+        //     $copies2 = 4;
+        //     $id2 = 2;
+        //     $test_book2 = new Book($title2, $copies2, $id2);
+        //     $test_book2->save();
+        //
+        //     $title3 = "Amber Spyglass";
+        //     $copies3 = 3;
+        //     $test_book3 = new Book($title3, $copies3);
+        //     $test_book3->save();
+        //
+        //     //Act
+        //     $test_author->addBook($test_book);
+        //     $test_author->addBook($test_book2);
+        //     $test_author->addBook($test_book3);
+        //
+        //     //Assert
+        //     $this->assertEquals([$test_book, $test_book2, $test_book3], $test_author->getBooks());
+        //
+        // }
+
+    }
+
+ ?>
